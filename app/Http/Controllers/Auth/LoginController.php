d805b07b789bbb3c281f349e04a93644ea6de2de
ChebuRawka <fxckn@mail.ru>
2024-06-17 23:45:11 +0300

    +reg+log

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    protected function loggedOut(Request $request)
+    {
+        return redirect('/');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <title>Новые Бизнес Системы</title>
     <link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet" />
     <link href="https://fonts.cdnfonts.com/css/unbounded" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
@@ -22,11 +23,34 @@
 
                 </ul>
             </nav>
+            
+<div class="personality">
+    @auth
+        <!-- Показать имя пользователя и кнопку выхода -->
+        <a href="{{ route('dashboard') }}" class="dashboard-btn"><img class="dashboard-btn-img" src="images/profile.png"></a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: inline;">
+            @csrf
+            <button type="submit" class="logout-btn"><img class="logout-btn-img" src="images/logout.png"></button>
+        </form>
+{{--        <a href="#" class="username">{{ Auth::user()->name }}!</a>--}}
 
-            <button class="reg-btn">Зарегистрироваться</button>
+
+
+    @else
+            <a class="reg-btn" href="{{ route('register') }}"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+                </svg></a>
+            <a class="log-btn" href="{{ route('login') }}"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                </svg></a>
+    @endauth
+
+</div>
             <div class="social-web">
-                <img   src="images/TelegramHeader.png">
-                <img  src="images/WhatsAppHeader.png">
+                <a  href="https://vk.com/ulik_nusht"><img  class="vkontakte" src="images/vk.svg"></a>
+               <a href="https://web.telegram.org/k/#@notcoin"><img class="telegram"  src="images/telegram.svg"></a>
             </div>
         </div>
     </div>
@@ -173,7 +197,7 @@
 </section>
 <footer>
         <div class="footer-text">
-        <img src="images/logo_dark.png" alt="NBS" class="logo">
+        <img src="images/logo_dark.png" alt="NBS" class="footer-logo">
         <p>Команда наших специалистов имеет огромный опыт работы с различными ситуациями. Мы обладаем широким набором различных инструментов для решения различных задача в сфере финансов, налогового права и экономической безопасности. Основная задача нашей компании – быть максимально эффективными для Вашего бизнеса."</p>
 
             <div class="footer-contact">
@@ -189,6 +213,7 @@
         <p>© 2020. <span class="highlight">ООО «Новые Бизнес Системы»</span> , ИНН 7723452322, КПП 772501001, ОГРН 1167746558407</p>
     </div>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <script src="script.js"></script>
 </body>
 </html>
